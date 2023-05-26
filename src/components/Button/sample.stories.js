4efcc6e745530a0a03fec46938ff46a136eb5085
yk-mt12 <agvksl543@gmail.com>
2023-05-26 12:40:37 +0900

    chore: hasky, lintstagedの設定を追加

---
+++ src/components/Button/sample.stories.js
@@ -0,0 +1,13 @@
+import Button from "./sample";
+
+export default {
+  title: "/Components/Button",
+  component: Button,
+};
+
+const Template = (args) => <Button {...args} />;
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: "Button",
+};
