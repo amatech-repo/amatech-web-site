c7dcad3bfbe90bea685098d9235ef4812b66ecd4
maple-fm <fuma.deguchi@gmail.com>
2023-06-02 16:41:28 +0900

    chore: tailwindとstorybookの接続

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // 'browser'フィールドを削除する
+    delete config.resolve.alias["browser"];
+
+    if (!isServer) {
+      config.node = {
+        fs: "empty",
+        net: "empty",
+        tls: "empty",
+      };
+    }
+
+    return config;
+  },
+};
