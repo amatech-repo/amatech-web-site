ec6c10d937501e08729cb108e095e3e5046616f9
kitsune <67742985+yk-mt12@users.noreply.github.com>
2023-06-09 03:38:38 +0900

    Delete HoldingFormatCard.stories.js

--- src/components/HoldingFormat/HoldingFormatCard.stories.js
+++
@@ -1,9 +0,0 @@
-import HoldingFormatCard from "./HoldingFormatCard";
-
-export default {
-  title: "Components/HoldingFormatCard",
-  component: HoldingFormatCard,
-};
-
-const Template = () => <HoldingFormatCard {...args} />;
-export const Default = Template.bind({});
