9c4c1c9c9577378c628a79dcf98855335f941790
kitsune <67742985+yk-mt12@users.noreply.github.com>
2023-06-09 03:40:00 +0900

    Delete index.stories.js

--- src/components/HoldingFormat/index.stories.js
+++
@@ -1,9 +0,0 @@
-import { HoldingFormat } from "./index";
-
-export default {
-  title: "Components/HoldingFormat",
-  component: HoldingFormat,
-};
-
-const Template = () => <HoldingFormat {...args} />;
-export const Default = Template.bind({});
