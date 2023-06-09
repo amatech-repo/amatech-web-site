dd635463b667d32e188a6f58a7175c23edc543a5
maple-fm <fuma.deguchi@gmail.com>
2023-06-10 01:49:07 +0900

    feat: ロールの有無を追加

--- src/components/Member/index.tsx
+++ src/components/Member/index.tsx
@@ -18,22 +18,29 @@ export const Member = (props: Props) => {
       <Image src={imagePath} alt="" width={95} height={95} className="pr-5 " />
       <div className="pt-8">
         <p className=" font-bold text-base">{name}</p>
+
         <div className="flex flex-row my-2">
-          <p
-            className="
-            text-amatech-log
-            text-xs
-            w-12
-            mr-2
-            border
-            rounded-full
-            flex
-            justify-center
-            bg-background-object
-            y-2"
-          >
-            {role}
-          </p>
+          {role ? (
+            <p
+              className="
+          text-amatech-log
+          text-xs
+          w-12
+          mr-2
+          border
+          rounded-full
+          flex
+          justify-center
+          bg-background-object
+          y-2
+          px-1"
+            >
+              {role}
+            </p>
+          ) : (
+            <></>
+          )}
+
           <p className="text-gray text-sm ">{grade}</p>
         </div>
         <div className="flex flex-row space-x-2 w-fit h-8 px-2 justify-center items-center rounded-32 bg-white drop-shadow-lg">
