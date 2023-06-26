a9dd121ada0595dc50922f534290f50a90bfaa79
Kazuma Ito <zarigani2x@gmail.com>
2023-06-27 00:49:56 +0900

    feat: フッターを実装

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,63 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export const Footer = () => {
-  return <h1 className="bg-blue-100 p-10">Footer</h1>;
+  return (
+    <footer className="bg-white grid grid-cols-12 pt-12 pb-4">
+      <Link href="/" className="row-start-1 col-start-3">
+        <Image alt="" src="/logo/amatech.svg" width={100} height={100} />
+      </Link>
+      <div className="flex flex-col items-center gap-8 col-span-full row-start-1 w-max mx-auto pb-12">
+        <div className="flex flex-col gap-2 text-xs w-max text-amatech-log font-bold font-YuGothic tracking-wider">
+          <Link href="/about" className="hover:underline">
+            あまてくとは？
+          </Link>
+          <Link href="/" className="hover:underline">
+            なんらかのリンク
+          </Link>
+          <Link href="/" className="hover:underline">
+            どこかへのリンク
+          </Link>
+        </div>
+        <hr className="h-[3px] w-[140px] rounded-full border-0 bg-slate-200" />
+        <div className="flex gap-6">
+          <LinkIcon
+            url="https://twitter.com/amatech1006"
+            img="/icons/twitter.svg"
+            alt="Twitter"
+          />
+          <LinkIcon
+            url="https://instagram.com/amatech1006"
+            img="/icons/instagram.svg"
+            alt="Instagram"
+          />
+          <LinkIcon
+            url="https://github.com/amatech-repo"
+            img="/icons/github.svg"
+            alt="GitHub"
+          />
+          <LinkIcon
+            url="https://qiita.com/organizations/amatech"
+            img="/icons/qiita.svg"
+            alt="Qiita"
+          />
+        </div>
+      </div>
+      <div className="row-start-2 col-start-1 col-end-[-1] text-center text-xs font-YuGothic text-gray font-bold">
+        あまてく © 2023
+      </div>
+    </footer>
+  );
 };
+
+function LinkIcon(props: { url: string; img: string; alt: string }) {
+  return (
+    <Link
+      href={props.url}
+      className="hover:scale-110 active:scale-100 transition"
+      target="_blank"
+    >
+      <Image alt={props.alt} src={props.img} width={30} height={30} />
+    </Link>
+  );
+}
