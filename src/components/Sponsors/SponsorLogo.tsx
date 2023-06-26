6dd8e7626ee5217abd2fd66fe887604879d82542
K-Ryo-ta <studyprograming51@gmail.com>
2023-06-26 23:27:30 +0900

    fix:divタグを減らしました。

--- src/components/Sponsors/SponsorLogo.tsx
+++ src/components/Sponsors/SponsorLogo.tsx
@@ -7,10 +7,14 @@ type Props = {
 export const SponsorLogo = ({ imagePath }: Props) => {
   return (
     <div>
-      <div className="w-[196.63px] h-[196.63px] bg-white rounded-[32px] shadow">
-        <div className="w-[160.55px] h-[116px] p-4 flex items-center justify-center">
-          <Image src={imagePath} alt="" width={160.55} height={116} />
-        </div>
+      <div className="w-[196.63px] h-[196.63px] bg-white rounded-[32px] shadow-2xl flex items-center justify-center">
+        <Image
+          src={imagePath}
+          alt=""
+          width={160.55}
+          height={116}
+          className="w-[160.55px] h-[116px] p-4"
+        />
       </div>
     </div>
   );
