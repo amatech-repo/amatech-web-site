9d5082f26bb9a9267e12fad2824d4024e028d11f
Kazuma Ito <zarigani2x@gmail.com>
2023-07-10 23:25:06 +0900

    feat: storiesを追加

---
+++ src/components/Footer/footer.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import  Footer  from "./index";
+
+const meta: Meta<typeof Footer> = {
+  title: "./Components/Footer",
+  component: Footer,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Footer>;
+
+export const Primary: Story = {
+  args: {},
+};
