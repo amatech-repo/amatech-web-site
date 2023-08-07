008bd77edf20a3ee5afa4378a436ad13970013d9
K-Ryo-ta <studyprograming51@gmail.com>
2023-08-07 22:42:01 +0900

    fix:余分なdivタグとclassNameの削除

--- src/components/Sponsors/SponsorLogo.tsx
+++ src/components/Sponsors/SponsorLogo.tsx
@@ -6,16 +6,8 @@ type Props = {
 
 export const SponsorLogo = ({ imagePath }: Props) => {
   return (
-    <div>
-      <div className="w-[196px] h-[196px] bg-white rounded-[32px] shadow-2xl flex items-center justify-center">
-        <Image
-          src={imagePath}
-          alt=""
-          width={160}
-          height={116}
-          className="w-[160px] h-[116px] p-4"
-        />
-      </div>
+    <div className="w-[196px] h-[196px] bg-white rounded-[32px] shadow-2xl flex items-center justify-center">
+      <Image src={imagePath} alt="" width={160} height={116} />
     </div>
   );
 };
