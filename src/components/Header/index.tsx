0a1513f9d98d1659f1179c8c25787f3d9e9cd2d2
maple-fm <fuma.deguchi@gmail.com>
2023-08-12 14:42:30 +0900

    feat: Headerの背景部分の作成

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,7 @@ import { Navigation } from "../Navigation";
 
 export const Header = () => {
   return (
-    <header className="bg-red-100 p-10">
-      <h1 className="">Header</h1>
+    <header className="bg-white py-5 px-9 w-full h-16 rounded-3xl drop-shadow-md">
       <Navigation />
     </header>
   );
