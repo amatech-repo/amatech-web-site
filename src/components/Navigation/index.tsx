3bc1d91067b121d55dad9a5f0c355a25cbb71efc
maple-fm <fuma.deguchi@gmail.com>
2023-08-15 10:45:05 +0900

    update: アイコンと文字の間にスペースを確保

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -11,23 +11,23 @@ export const Navigation = () => {
           </Link>
         </li>
         <div className="flex justify-end items-center ml-auto space-x-10">
-          <li className="flex flex-row m-2">
+          <li className="flex flex-row space-x-2">
             <Image src={"/icons/news.svg"} alt={""} width={24} height={24} />
             <Link href="/news">ニュース</Link>
           </li>
-          <li className="flex flex-row m-2">
+          <li className="flex flex-row space-x-2">
             <Image src={"/icons/sponsor.svg"} alt={""} width={24} height={24} />
             <Link href="/sponsor">スポンサー</Link>
           </li>
-          <li className="flex flex-row m-2">
+          <li className="flex flex-row space-x-2">
             <Image src={"/icons/member.svg"} alt={""} width={24} height={24} />
             <Link href="/member">運営メンバー</Link>
           </li>
-          <li className="flex flex-row m-2">
+          <li className="flex flex-row space-x-2">
             <Image src={"/icons/faq.svg"} alt={""} width={24} height={24} />
             <Link href="/faq">FAQ</Link>
           </li>
-          <li className="flex flex-row m-2">
+          <li className="flex flex-row space-x-2">
             <Image src={"/icons/contact.svg"} alt={""} width={24} height={24} />
             <Link href="/contact">お問い合わせ</Link>
           </li>
