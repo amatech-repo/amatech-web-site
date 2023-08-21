f4c90474a89f0d7f2b7adb123ce83397543156f1
K-Ryo-ta <studyprograming51@gmail.com>
2023-08-21 23:42:23 +0900

    style:画像を透けるように設定した。テキストの大きさを調整中。

--- src/components/HomepageTop/HomepageTop.tsx
+++ src/components/HomepageTop/HomepageTop.tsx
@@ -6,12 +6,13 @@ type Props = {
   imagePath3: string;
 };
 export const HomepageTop = ({ imagePath1, imagePath2, imagePath3 }: Props) => {
+  const text: string = "エンジニア\nコミュニティ";
   return (
     <div>
       <Image
         src={imagePath3}
         alt=""
-        className="w-[1318px] h-[1270px]"
+        className="w-[1280px] h-[1230px]"
         quality={100}
         fill
         sizes="100vw"
@@ -19,30 +20,30 @@ export const HomepageTop = ({ imagePath1, imagePath2, imagePath3 }: Props) => {
           objectFit: "cover",
         }}
       />
-      <div className="w-[1280px] h-[825px]  blur-[0px] flex place-content-center ml-4">
-        <div>
-          <div className="text-neutral-900 text-[50px] font-extrabold font-concept tracking-widest">
+      <div className="flex  ml-4">
+        <div className="blur-[0px]">
+          <div className=" text-neutral-900 text-[40px] pt-[40px] font-extrabold font-concept tracking-widest">
             どうせやるなら、
             <br />
             強くなろう。
           </div>
           <div className="w-[428px]">
-            <span className="text-zinc-800 text-base font-bold tracking-wide">
+            <span className="text-zinc-800 text-[14px] font-bold tracking-wide">
               あまてくは京都産業大学の学生を中心とした
             </span>
-            <span className="text-sky-400 text-base font-bold tracking-wide">
-              エンジニアコミュニティ
+            <span className="text-sky-400 text-[14px] font-bold tracking-wide">
+              {text}
             </span>
-            <span className="text-zinc-800 text-base font-bold tracking-wide">
+            <span className="text-zinc-800 text-[14px] font-bold tracking-wide">
               です。
               <br />
               <br />
               勉強会やLT会など、授業で触れる以外のプログラミングや技術のキャッチアップだけでなく、ハッカソン出場やチームメンバーの育成など
             </span>
-            <span className="text-sky-400 text-base font-bold tracking-wide">
+            <span className="text-sky-400 text-[14px] font-bold tracking-wide">
               エンジニアを目指すきっかけ
             </span>
-            <span className="text-zinc-800 text-base font-bold tracking-wide">
+            <span className="text-zinc-800 text-[14px] font-bold tracking-wide">
               になる活動を行なっています。
             </span>
           </div>
@@ -65,7 +66,7 @@ export const HomepageTop = ({ imagePath1, imagePath2, imagePath3 }: Props) => {
           alt=""
           width={674}
           height={483}
-          className="w-[673px] h-[483px] p-4"
+          className="w-[674px] h-[483px] pr-[200px] pb-[50px]"
         />
       </div>
     </div>
