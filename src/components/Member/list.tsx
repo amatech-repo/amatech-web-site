b96d630943121a9f36ed5fd16358cd88cfa89384
kitsune <agvksl543@gmail.com>
2023-10-25 22:59:07 +0900

    chore: fixed build error for sb

--- src/components/Member/list.tsx
+++ src/components/Member/list.tsx
@@ -9,18 +9,19 @@ type MemberListProps = {
 export const MemberList = ({ members }: MemberListProps) => {
   return (
     <div className="flex flex-col items-center space-y-12 sm:flex-row sm:space-y-0 sm:space-x-12">
-      {members?.contents.map((member: MembersType, index: number) => (
-        <Member
-          key={index}
-          imageUrl={member.imageUrl ? member.imageUrl.url : ""}
-          name={member.name}
-          grade={member.age.join(", ")}
-          role={member.role.join(", ")}
-          github={member.github}
-          twitter={""}
-          instagram={""}
-        />
-      ))}
+      {members &&
+        members?.contents.map((member: MembersType, index: number) => (
+          <Member
+            key={index}
+            imageUrl={member.imageUrl ? member.imageUrl.url : ""}
+            name={member.name}
+            grade={member.age.join(", ")}
+            role={member.role.join(", ")}
+            github={member.github}
+            twitter={""}
+            instagram={""}
+          />
+        ))}
     </div>
   );
 };
--- src/components/Sponsors/SponsorLogoList.tsx
+++ src/components/Sponsors/SponsorLogoList.tsx
@@ -9,12 +9,13 @@ type SponsorListProps = {
 export const SponsorLogoList = ({ sponsors }: SponsorListProps) => {
   return (
     <div className="flex flex-col sm:flex-row gap-[42px] items-center w-full">
-      {sponsors?.contents.map((sponsor, index) => (
-        <SponsorLogo
-          key={index}
-          ImagePath={sponsor.url?.url ? sponsor.url.url : undefined}
-        />
-      ))}
+      {sponsors &&
+        sponsors?.contents.map((sponsor, index) => (
+          <SponsorLogo
+            key={index}
+            ImagePath={sponsor.url?.url ? sponsor.url.url : undefined}
+          />
+        ))}
     </div>
   );
 };
