2ab2118dc3277f6af702e4f5ce2de33cea9123d5
kitsune <agvksl543@gmail.com>
2023-10-25 23:09:49 +0900

    chore: fixed build error for storybook

--- src/components/Member/list.tsx
+++ src/components/Member/list.tsx
@@ -7,21 +7,22 @@ type MemberListProps = {
 };
 
 export const MemberList = ({ members }: MemberListProps) => {
+  // members.contentsがundefinedの場合は空の配列を返す
+  const contents = members?.contents ?? [];
   return (
     <div className="flex flex-col items-center space-y-12 sm:flex-row sm:space-y-0 sm:space-x-12">
-      {members &&
-        members?.contents.map((member: MembersType, index: number) => (
-          <Member
-            key={index}
-            imageUrl={member.imageUrl ? member.imageUrl.url : ""}
-            name={member.name}
-            grade={member.age.join(", ")}
-            role={member.role.join(", ")}
-            github={member.github}
-            twitter={""}
-            instagram={""}
-          />
-        ))}
+      {contents.map((member: MembersType, index: Key) => (
+        <Member
+          key={index}
+          imageUrl={member.imageUrl ? member.imageUrl.url : ""}
+          name={member.name}
+          grade={member.age.join(", ")}
+          role={member.role.join(", ")}
+          github={member.github}
+          twitter={""}
+          instagram={""}
+        />
+      ))}
     </div>
   );
 };
