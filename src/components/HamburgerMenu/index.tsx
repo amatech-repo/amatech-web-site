baee8d495752e0260cbf63c3f3debf252ffadfb7
maple-fm <fuma.deguchi@gmail.com>
2023-10-27 23:55:42 +0900

    update: 位置の修正とアイコンの表示

--- src/components/HamburgerMenu/index.tsx
+++ src/components/HamburgerMenu/index.tsx
@@ -11,15 +11,15 @@ export const HamburgerMenu = () => {
   };
 
   return (
-    <nav>
-      <ul className="flex flex-row space-x-auto h-6">
-        <li className="flex justify-start items-center">
+    <nav className="relative">
+      <ul className="flex flex-row space-x-auto h-6 justify-between items-center">
+        <li>
           <Link href="/" className="text-amatech-log">
             あまてく
           </Link>
         </li>
 
-        <div className="relative flex justify-end items-center ml-auto space-x-10">
+        <li className="relative flex justify-end items-center ml-auto space-x-10">
           <button onClick={toggleMenu} className="focus:outline-none">
             <Image
               src="/icons/hamburger.svg"
@@ -30,36 +30,36 @@ export const HamburgerMenu = () => {
           </button>
 
           {isOpen && (
-            <div className="absolute top-[-10px] left-[-200px] bg-white border rounded-lg p-4 w-[200px] shadow-lg">
+            <div className="absolute top-full right-0 mt-2 bg-white border rounded-lg p-4 w-[200px] shadow-lg">
               <ul className="space-y-3">
-                <li>
+                <li className="flex items-center space-x-2">
                   <Image
-                    src="/icons/home.svg"
-                    alt="ホーム"
+                    src="/icons/news.svg"
+                    alt="ニュース"
                     width={24}
                     height={24}
                   />
-                  <Link href="/">ホーム</Link>
+                  <Link href="/">ニュース</Link>
                 </li>
-                <li>
+                <li className="flex items-center space-x-2">
                   <Image
-                    src="/icons/heart.svg"
+                    src="/icons/sponsor.svg"
                     alt="スポンサー"
-                    width={24}
-                    height={24}
+                    width={20}
+                    height={20}
                   />
                   <Link href="/sponsor">スポンサー</Link>
                 </li>
-                <li>
+                <li className="flex items-center space-x-2">
                   <Image
-                    src="/icons/user.svg"
+                    src="/icons/member.svg"
                     alt="運営メンバー"
                     width={24}
                     height={24}
                   />
                   <Link href="/member">運営メンバー</Link>
                 </li>
-                <li>
+                <li className="flex items-center space-x-2">
                   <Image
                     src="/icons/faq.svg"
                     alt="FAQ"
@@ -68,7 +68,7 @@ export const HamburgerMenu = () => {
                   />
                   <Link href="/faq">FAQ</Link>
                 </li>
-                <li>
+                <li className="flex items-center space-x-2">
                   <Image
                     src="/icons/contact.svg"
                     alt="お問い合わせ"
@@ -80,7 +80,7 @@ export const HamburgerMenu = () => {
               </ul>
             </div>
           )}
-        </div>
+        </li>
       </ul>
     </nav>
   );
