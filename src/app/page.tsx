6da2aee3007a1b50ebeac040a80c1247b3718fe0
maple-fm <fuma.deguchi@gmail.com>
2023-11-02 00:10:57 +0900

    feat: プロップスの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import { MemberList } from "@/components/Member/list";
 import { MembersType, MembersListType } from "@/types/member";
 import { SponsorLogoList } from "@/components/Sponsors/SponsorLogoList";
 import { Top } from "@/components/Top";
+import { ImageSize, SectionTitle } from "@/components/SectionTitle";
 
 export default async function Home() {
   const admin = await getAdminList();
@@ -32,9 +33,19 @@ export default async function Home() {
         <HoldingFormat />
       </section>
       <section className="mt-20">
+        <SectionTitle
+          title={"Admin"}
+          subtitle={"運営メンバー"}
+          image={ImageSize.small}
+        />
         <MemberList members={admin ? admin : undefined} />
       </section>
       <section className="mt-20 mb-10">
+        <SectionTitle
+          title={"Sponsors"}
+          subtitle={"協賛・スポンサー企業"}
+          image={ImageSize.small}
+        />
         <SponsorLogoList sponsors={sponsors ? sponsors : undefined} />
       </section>
     </main>
--- src/components/SectionTitle/index.tsx
+++ src/components/SectionTitle/index.tsx
@@ -1,19 +1,33 @@
 import React from "react";
 import Image from "next/image";
 
-export const SectionTitle = () => {
+type Props = {
+  title: string;
+  subtitle: string;
+  image: ImageSize;
+};
+
+export enum ImageSize {
+  small = "small.svg",
+  medium = "medium.svg",
+  large = "large.svg",
+}
+
+export const SectionTitle = (props: Props) => {
+  const { title, subtitle, image } = props;
+
   return (
-    <div className="flex items-center justify-center relative w-full h-full">
+    <div className="flex items-center justify-center relative w-full h-full mb-10">
       <Image
-        src="./background/sections/small.svg"
+        src={`./background/sections/${image}`}
         alt=""
         width={1}
         height={1}
         className="w-1/6 z-10"
       />
       <div className="text-title absolute z-20 text-center w-full">
-        <div className="text-5xl font-bold">Admin</div>
-        <div className="text-lg">運営メンバー</div>
+        <div className="text-5xl font-bold">{title}</div>
+        <div className="text-lg">{subtitle}</div>
       </div>
     </div>
   );
