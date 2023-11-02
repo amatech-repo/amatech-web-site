8a89cfa98a40907eb6f2ed2c86cbfb07f641236b
kitsune <agvksl543@gmail.com>
2023-11-03 00:13:22 +0900

    refactor: fixed build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
 import { getAdminList } from "@/utils/getAdmin";
 import { getNewsList } from "@/utils/getNews";
 import { getSponsorList } from "@/utils/getSponsor";
@@ -38,12 +34,6 @@ export default async function Home() {
   const admin = await getAdminList();
   const news = await getNewsList();
   const sponsors = await getSponsorList();
-
-  // TODO: 型指定をする
-  // console.log('member', member);
-  console.log("########### news ########", news);
-  console.log("######## sponsor ########", sponsors);
-  // console.log("########### admin ########", admin);
 
   return (
     <main>
--- src/components/News/NewsList.tsx
+++ src/components/News/NewsList.tsx
@@ -8,46 +8,47 @@ type NewsCardListProps = {
 };
 
 export const NewsList = ({ newsCards }: NewsCardListProps) => {
-  const contetns = newsCards?.contents ?? [];
+  const contents = newsCards?.contents && [];
   return (
     <div className=" w-full h-[50rem] my-10">
       <div className="absolute">
-        {contetns.map((newsCard: NewsType, index: Key) => (
-          <>
-            <div>
-              <div className="flex flex-col justify-center w-5/6 items-center">
-                <div className="relative">
-                  {/* Dot Image (Centered) */}
-                  <Image
-                    src={"./images/dot.svg"}
-                    alt=""
-                    width={28}
-                    height={28}
-                    className="absolute top-0 left-2 right-2 bottom-0 mx-auto"
-                  />
+        {contents &&
+          contents.map((newsCard: NewsType, index: Key) => (
+            <>
+              <div>
+                <div className="flex flex-col justify-center w-5/6 items-center">
+                  <div className="relative">
+                    {/* Dot Image (Centered) */}
+                    <Image
+                      src={"./images/dot.svg"}
+                      alt=""
+                      width={28}
+                      height={28}
+                      className="absolute top-0 left-2 right-2 bottom-0 mx-auto"
+                    />
 
-                  {/* Date (Right of the Dot) */}
-                  <div className=" ml-[112px] mb-[15px] pb-5 font-YuGothic font-bold text-2xl text-cyan-950">
-                    {newsCard.eventDate
-                      .split("T")[0]
-                      .split("-")
-                      .slice(1)
-                      .join("/")}
+                    {/* Date (Right of the Dot) */}
+                    <div className=" ml-[112px] mb-[15px] pb-5 font-YuGothic font-bold text-2xl text-cyan-950">
+                      {newsCard.eventDate
+                        .split("T")[0]
+                        .split("-")
+                        .slice(1)
+                        .join("/")}
+                    </div>
                   </div>
-                </div>
 
-                {/* NewsCard */}
-                <NewsCard
-                  key={index}
-                  title={newsCard.title}
-                  place={newsCard.place}
-                  imageURL={newsCard.imageURL.url ?? ""}
-                  isRight={true}
-                />
+                  {/* NewsCard */}
+                  <NewsCard
+                    key={index}
+                    title={newsCard.title}
+                    place={newsCard.place}
+                    imageURL={newsCard.imageURL.url ?? ""}
+                    isRight={true}
+                  />
+                </div>
               </div>
-            </div>
-          </>
-        ))}
+            </>
+          ))}
       </div>
       <div className="flex justify-center">
         <div className="absolute">
