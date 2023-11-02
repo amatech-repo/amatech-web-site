c496a6396837aa938982a555ef5f6e8fe4cfa57e
maple-fm <fuma.deguchi@gmail.com>
2023-11-03 01:28:18 +0900

    update: レスポンシブの対応

--- src/components/SNSCard/list.tsx
+++ src/components/SNSCard/list.tsx
@@ -3,7 +3,7 @@ import { SNSCard } from ".";
 
 export const SNSCardList = () => {
   return (
-    <div className="flex flex-row justify-center gap-5">
+    <div className="flex sm:flex-row flex-col justify-center items-center gap-5">
       <SNSCard snsName={"github"} id={"amatech-repo"} name={"GitHub"} />
       <SNSCard snsName={"twitter"} id={"amatech1006"} name={"Twitter"} />
       <SNSCard snsName={"qiita"} id={"amatech1006"} name={"Qiita"} />
