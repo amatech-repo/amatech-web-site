ed037393668341477d6770f4a80360ebdd6d2b22
maple-fm <fuma.deguchi@gmail.com>
2023-11-03 00:54:54 +0900

    feat: SNSCardにpropsを追加

--- src/components/SNSCard/index.tsx
+++ src/components/SNSCard/index.tsx
@@ -2,13 +2,25 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-export const SNSCard = () => {
+type Props = {
+  snsName: string; // url, svgに使用
+  id: string; // snsID
+  name: string; // SNS名
+};
+
+export const SNSCard = (props: Props) => {
+  const { snsName, id, name } = props;
   return (
-    <Link href="https://github.com/amatech-repo">
+    <Link href={"https://" + snsName + ".com/" + id}>
       <div className="w-48 h-48 relative drop-shadow-lg bg-white rounded-32">
         <div className="w-full h-full flex flex-col justify-center items-center">
-          <Image src={"./icons/github.svg"} alt="" width={76} height={76} />
-          <div className="mt-2 text-primary font-bold text-xl">GitHub</div>
+          <Image
+            src={"/icons/" + snsName + ".svg"}
+            alt=""
+            width={76}
+            height={76}
+          />
+          <div className="mt-2 text-primary font-bold text-xl">{name}</div>
         </div>
       </div>
     </Link>
