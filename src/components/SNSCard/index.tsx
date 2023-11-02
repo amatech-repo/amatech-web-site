b474ab0298b38885d9fae0c71a7d9bfdb41d885f
maple-fm <fuma.deguchi@gmail.com>
2023-11-03 00:46:23 +0900

    update: UIを修正

--- src/components/SNSCard/index.tsx
+++ src/components/SNSCard/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export const SNSCard = () => {
   return (
-    <div className="w-48 h-48 relative drop-shadow-lg ">
-      <div className="w-24 h-24 bg-white flex flex-col justify-center items-center rounded-lg">
-        <Image src={"./icons/github.svg"} alt="" width={32} height={32} />
-        <div className="mt-2 text-primary font-bold">GitHub</div>
+    <Link href="https://github.com/amatech-repo">
+      <div className="w-48 h-48 relative drop-shadow-lg bg-white rounded-32">
+        <div className="w-full h-full flex flex-col justify-center items-center">
+          <Image src={"./icons/github.svg"} alt="" width={76} height={76} />
+          <div className="mt-2 text-primary font-bold text-xl">GitHub</div>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
