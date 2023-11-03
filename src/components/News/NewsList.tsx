2c6e602746e2cbd26589c833d53270ba9d1dd896
kitsune <agvksl543@gmail.com>
2023-11-03 23:57:14 +0900

    style: Updated news ui

--- src/components/News/NewsList.tsx
+++ src/components/News/NewsList.tsx
@@ -10,15 +10,15 @@ type NewsCardListProps = {
 export const NewsList = ({ newsCards }: NewsCardListProps) => {
   const contents = newsCards?.contents ?? [];
   return (
-    <div className="relative w-full my-10">
+    <div className="relative w-full">
       {/* News Cards and Dots */}
       <div className="flex flex-col items-center relative z-10">
         {contents.map((newsCard: NewsType, index) => (
           <div
             key={index}
-            className="flex flex-col items-center w-5/6 mb-8 relative"
+            className="flex flex-col items-center w-5/6 mb-20 relative"
           >
-            <div className="flex items-center mb-2 relative text-center w-full">
+            <div className="flex items-center mb-8 relative text-center w-full">
               {/* Dot Image */}
               <div className="absolute left-1/2 transform -translate-x-1/2">
                 <Image
@@ -30,13 +30,48 @@ export const NewsList = ({ newsCards }: NewsCardListProps) => {
                 />
               </div>
 
-              {/* Date (Left of the Dot) */}
-              <div
-                className="font-YuGothic font-bold text-2xl text-cyan-950 absolute"
-                style={{ right: "50%", marginRight: "28px" }}
-              >
-                {newsCard.eventDate.split("T")[0].split("-").slice(1).join("/")}
-              </div>
+              {/* Date */}
+              {index % 2 === 0 ? (
+                <div
+                  className="font-YuGothic font-bold text-2xl text-cyan-950 absolute"
+                  style={{ right: "50%", marginRight: "32px" }}
+                >
+                  <span>
+                    {newsCard.eventDate
+                      .split("T")[0]
+                      .split("-")
+                      .slice(1)
+                      .join("/")}
+                  </span>
+                  <Image
+                    src={"/images/polygon.svg"}
+                    alt=""
+                    width={28}
+                    height={28}
+                    className="relative top-6 left-1/4"
+                  />
+                </div>
+              ) : (
+                <div
+                  className="font-YuGothic font-bold text-2xl text-cyan-950 absolute"
+                  style={{ left: "50%", marginLeft: "32px" }}
+                >
+                  <span>
+                    {newsCard.eventDate
+                      .split("T")[0]
+                      .split("-")
+                      .slice(1)
+                      .join("/")}
+                  </span>
+                  <Image
+                    src={"/images/polygon.svg"}
+                    alt=""
+                    width={28}
+                    height={28}
+                    className="relative top-6 left-1/4"
+                  />
+                </div>
+              )}
             </div>
 
             {/* NewsCard */}
@@ -46,6 +81,17 @@ export const NewsList = ({ newsCards }: NewsCardListProps) => {
               imageURL={newsCard.imageURL.url ?? ""}
               isRight={true}
             />
+
+            {/* もっと見るボタン */}
+            {index === 2 ? (
+              <div className="flex justify-center mt-32">
+                <button className="w-full h-full bg-white rounded-full border-4 border-background-object px-7 py-3">
+                  もっと見る
+                </button>
+              </div>
+            ) : (
+              <></>
+            )}
           </div>
         ))}
       </div>
@@ -56,7 +102,7 @@ export const NewsList = ({ newsCards }: NewsCardListProps) => {
         alt=""
         width={10}
         height={10}
-        className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0 sm:h-[1100px] h-[1500px]"
+        className="absolute left-1/2 top-[46%] sm:top-[57%]  transform -translate-x-1/2 -translate-y-1/2 z-0 sm:h-[1800px] h-[1800px]"
       />
     </div>
   );
