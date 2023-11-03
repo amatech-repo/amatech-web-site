adbfd268615f2f0d0e9e57b8e37dabc9e04a370d
kitsune <agvksl543@gmail.com>
2023-11-04 04:41:32 +0900

    style: newsCardを修正

--- src/components/News/NewsCard.tsx
+++ src/components/News/NewsCard.tsx
@@ -12,24 +12,32 @@ export const NewsCard = (props: Props) => {
   const { title, place, imageURL, isRight } = props;
 
   return (
-    <>
-      <div className="w-5/6 mt-4 mb-8 ">
-        <div className="flex flex-col sm:flex-row sm: pb-10 shadow-lg rounded-md  border-spacing-1 border-black bg-news-background">
-          <Image src={imageURL} alt="" width={245} height={150}></Image>
-          <div className="ml-6 mt-3">
-            <div className="text-2xl font-bold mb-2">{title}</div>
-            <div className="flex flex-row">
-              <Image
-                src={"./images/mapping.svg"}
-                alt=""
-                width={24}
-                height={24}
-              />
-              <div className="text-neutral-400">{place}</div>
-            </div>
-          </div>
+    <div
+      className={`w-5/6 mt-4 mb-8 ${
+        isRight ? "md:flex-row-reverse" : "md:flex-row"
+      } flex flex-col md:flex-row bg-news-background rounded-lg shadow-lg overflow-hidden`}
+    >
+      <div className="relative w-full md:w-1/3 mb-4 md:mb-0">
+        <Image
+          src={imageURL}
+          width={245}
+          height={150}
+          alt=""
+          // TODO: ClipPathを使って画像の内側を丸く凹ませる
+        />
+      </div>
+      <div className="w-full md:w-2/3 p-6">
+        <div className="text-2xl font-bold mb-4">{title}</div>
+        <div className="flex flex-row items-center">
+          <Image
+            src={"./images/mapping.svg"}
+            alt="Mapping icon"
+            width={24}
+            height={24}
+          />
+          <div className="text-neutral-400 ml-4">{place}</div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
