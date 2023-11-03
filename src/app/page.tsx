6b40e8e6fd29cc1a817857b47b0c5e4766cd0359
kitsune <67742985+yk-mt12@users.noreply.github.com>
2023-11-04 02:00:23 +0900

    style: update top page style (#52)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,67 +37,70 @@ export default async function Home() {
 
   return (
     <>
-      <section className="mt-full">
+      <section className="max-w-4xl">
         <Top />
       </section>
-      <div className="bg-background">
-        <section className="mt-24">
-          <SectionTitle
-            title={"Holding Format"}
-            subtitle={"開催形式"}
-            image={ImageSize.small}
-          />
-          <HoldingFormat />
-        </section>
-        <section className="mt-24">
-          <SectionTitle
-            title={"Admin"}
-            subtitle={"運営メンバー"}
-            image={ImageSize.small}
-          />
-          <MemberList members={admin ? admin : undefined} />
-        </section>
-        <section className="mt-24">
-          <SectionTitle
-            title={"News"}
-            subtitle={"最新情報"}
-            image={ImageSize.small}
-          />
-          <NewsList newsCards={news ? news : undefined}></NewsList>
-        </section>
-        <section className="mt-24">
-          <SectionTitle
-            title={"Sponsors"}
-            subtitle={"協賛・スポンサー企業"}
-            image={ImageSize.small}
-          />
-          {/* @ts-ignore */}
-          <SponsorLogoList sponsors={sponsors ? sponsors : undefined} />
-        </section>
-        <section className="mt-24">
-          <SectionTitle
-            title={"Activities"}
-            subtitle={"活動内容"}
-            image={ImageSize.small}
-          />
-          <ActivityList />
-        </section>
-        <section className="mt-24">
-          <SectionTitle
-            title={"FAQ"}
-            subtitle={"よくある質問"}
-            image={ImageSize.small}
-          />
-          <FAQList faqs={faqs} />
-        </section>
-        <section className="mt-24">
-          <SectionTitle
-            title={"SNS"}
-            subtitle={"公式SNS"}
-            image={ImageSize.small}
-          />
-          <SNSCardList />
-        </section>
+      <div className="bg-background w-full mt-20 rounded-tl-[300px] rounded-tr-[300px]">
+        <div className="max-w-4xl mx-auto pb-32">
+          <section className="mt-24">
+            <SectionTitle
+              title={"Holding Format"}
+              subtitle={"開催形式"}
+              image={ImageSize.small}
+            />
+            <HoldingFormat />
+          </section>
+          <section className="mt-24">
+            <SectionTitle
+              title={"Admin"}
+              subtitle={"運営メンバー"}
+              image={ImageSize.small}
+            />
+            <MemberList members={admin ? admin : undefined} />
+          </section>
+          <section className="mt-24">
+            <SectionTitle
+              title={"News"}
+              subtitle={"最新情報"}
+              image={ImageSize.small}
+            />
+            <NewsList newsCards={news ? news : undefined}></NewsList>
+          </section>
+          <section className="mt-24">
+            <SectionTitle
+              title={"Sponsors"}
+              subtitle={"協賛・スポンサー企業"}
+              image={ImageSize.small}
+            />
+            {/* @ts-ignore */}
+            <SponsorLogoList sponsors={sponsors ? sponsors : undefined} />
+          </section>
+          <section className="mt-24">
+            <SectionTitle
+              title={"Activities"}
+              subtitle={"活動内容"}
+              image={ImageSize.small}
+            />
+
+            <ActivityList />
+          </section>
+          <section className="mt-24">
+            <SectionTitle
+              title={"FAQ"}
+              subtitle={"よくある質問"}
+              image={ImageSize.small}
+            />
+            <FAQList faqs={faqs} />
+          </section>
+          <section className="mt-24">
+            <SectionTitle
+              title={"SNS"}
+              subtitle={"公式SNS"}
+              image={ImageSize.small}
+            />
+            <SNSCardList />
+          </section>
+        </div>
       </div>
     </>
   );
