cc4b9233773a7f026490c55ff20ad6274d2d6878
Kazuma Ito <zarigani2x@gmail.com>
2023-11-04 22:18:05 +0900

    Newsのスタイルを更新

--- src/components/News/NewsCard.tsx
+++ src/components/News/NewsCard.tsx
@@ -12,30 +12,20 @@ export const NewsCard = (props: Props) => {
   const { title, place, imageURL, isRight } = props;
 
   return (
-    <div
-      className={`w-5/6 mt-4 mb-8  ${
-        isRight ? "md:flex-row-reverse" : "md:flex-row"
-      } flex flex-col md:flex-row bg-news-background rounded-3xl shadow-lg overflow-hidden`}
-    >
-      <div className="relative w-full md:w-1/3 mb-4 md:mb-0 mx-auto">
-        <Image
-          src={imageURL}
-          width={245}
-          height={150}
-          alt=""
-          // TODO: ClipPathを使って画像の内側を丸く凹ませる
-        />
+    <div className="shadow bg-white md:rounded-3xl flex flex-col w-full overflow-hidden md:flex-row-reverse z-10">
+      <div className="h-40 w-full min-h-full relative overflow-hidden bg-neutral-50">
+        <Image src={imageURL} width={800} height={800} alt="" />
       </div>
-      <div className="w-full md:w-2/3 p-6">
-        <div className="text-2xl font-bold mb-4">{title}</div>
-        <div className="flex flex-row items-center">
+      <div className="flex flex-col gap-3 justify-between p-6">
+        <div className="text-lg font-semibold">{title}</div>
+        <div className="flex items-center text-neutral-400 text-sm gap-1 -ml-1">
           <Image
             src={"./images/mapping.svg"}
             alt="Mapping icon"
-            width={24}
-            height={24}
+            width={18}
+            height={18}
           />
-          <div className="text-neutral-400 ml-4">{place}</div>
+          <div className="">{place}</div>
         </div>
       </div>
     </div>
--- src/components/News/NewsList.tsx
+++ src/components/News/NewsList.tsx
@@ -1,6 +1,7 @@
 import { NewsCard } from "./NewsCard";
 import Image from "next/image";
 import { NewsListType, NewsType } from "@/types/news";
+import Link from "next/link";
 
 type NewsCardListProps = {
   newsCards: NewsListType | undefined;
@@ -10,64 +11,41 @@ export const NewsList = ({ newsCards }: NewsCardListProps) => {
   const contents = newsCards?.contents ?? [];
   return (
     <div className="relative w-full">
-      <div className="flex flex-col items-center relative z-10">
-        {contents.map((newsCard: NewsType, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center w-5/6 mt-14 mb-20 relative"
-          >
-            <div className="flex items-center mb-8 relative text-center w-full">
-              <div className="absolute left-1/2 transform -translate-x-1/2">
-                <Image src={"/images/dot.svg"} alt="" width={28} height={28} />
-              </div>
-
-              <div
-                className={`font-YuGothic font-bold text-2xl text-cyan-950 absolute ${
-                  index % 2 === 0 ? "right-1/2 mr-8" : "left-1/2 ml-8"
-                }`}
-              >
-                <span>
-                  {newsCard.eventDate
-                    .split("T")[0]
-                    .split("-")
-                    .slice(1)
-                    .join("/")}
-                </span>
-                <Image
-                  src={"/images/polygon.svg"}
-                  alt=""
-                  width={28}
-                  height={28}
-                  className="relative top-6 left-1/4"
-                />
-              </div>
+      {contents.map((newsCard: NewsType, index) => (
+        <div
+          key={index}
+          className="flex flex-col items-center first:pt-10 mb-14 relative"
+        >
+          <div className="mb-6 items-center relative text-center w-full grid grid-cols-[auto,24px,auto]">
+            {/* dot */}
+            <div className="row-start-1 col-span-full flex justify-center">
+              <div className="w-6 h-6 bg-white rounded-full border-background-object border-[6px] z-40"></div>
             </div>
-
-            <NewsCard
-              title={newsCard.title}
-              place={newsCard.place}
-              imageURL={newsCard.imageURL.url ?? ""}
-              isRight={index % 2 === 0}
-            />
-
-            {index === contents.length - 1 ? (
-              <div className="flex justify-center mt-24">
-                <button className="bg-white rounded-full border-4 border-background-object px-7 py-3">
-                  もっと見る
-                </button>
-              </div>
-            ) : null}
+            {/* date */}
+            <div className="col-start-1 row-start-1 font-bold text-2xl">
+              {newsCard.eventDate.split("T")[0].split("-").slice(1).join("/")}
+            </div>
           </div>
-        ))}
+
+          <NewsCard
+            title={newsCard.title}
+            place={newsCard.place}
+            imageURL={newsCard.imageURL.url ?? ""}
+            isRight={index % 2 === 0}
+          />
+        </div>
+      ))}
+      <div className="flex">
+        <Link
+          href="/news"
+          className="bg-white rounded-full border-4 border-background-object px-7 py-3 mx-auto z-10"
+        >
+          もっと見る
+        </Link>
       </div>
 
-      <Image
-        src={"/icons/Ziku.svg"}
-        alt=""
-        width={10}
-        height={10}
-        className="absolute left-1/2 top-0 transform -translate-x-1/2 z-0  md:h-[1300px] h-[2000px]"
-      />
+      {/* vertical axis */}
+      <div className="absolute w-2 h-full bg-background-object rounded-full top-0 left-1/2 -translate-x-1/2 z-0"></div>
     </div>
   );
 };
