e4562079b31bbb4856d77ff455ada4a1f0c73e95
Kazuma Ito <zarigani2x@gmail.com>
2023-11-05 16:21:14 +0900

    mdサイズ以上のときのニュースカードを更新

--- src/components/News/NewsCard.tsx
+++ src/components/News/NewsCard.tsx
@@ -12,11 +12,11 @@ export const NewsCard = (props: Props) => {
   const { title, place, imageURL, isRight } = props;
 
   return (
-    <div className="shadow bg-white md:rounded-3xl flex flex-col w-full overflow-hidden md:flex-row-reverse z-10">
-      <div className="h-40 w-full min-h-full relative overflow-hidden bg-neutral-50">
+    <div className="shadow bg-white md:rounded-3xl flex flex-col w-full overflow-hidden md:flex-row-reverse z-10 md:justify-between">
+      <div className="h-40 w-full min-h-full relative overflow-hidden bg-neutral-50 md:w-1/3">
         <Image src={imageURL} width={800} height={800} alt="" />
       </div>
-      <div className="flex flex-col gap-3 justify-between p-6">
+      <div className="flex flex-col gap-3 justify-between p-6 md:p-10 md:w-2/3">
         <div className="text-lg font-semibold">{title}</div>
         <div className="flex items-center text-neutral-400 text-sm gap-1 -ml-1">
           <Image
