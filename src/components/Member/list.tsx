d7e52a6b015e1a03d25a52e8f25b198ce40e43f2
Kazuma Ito <zarigani2x@gmail.com>
2023-11-05 15:30:09 +0900

    Adminメンバーの無限スクロールを実装

--- src/components/Member/list.tsx
+++ src/components/Member/list.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Member } from "./index";
 import { MembersType, MembersListType } from "@/types/member";
 
@@ -7,33 +7,54 @@ type MemberListProps = {
   members: MembersListType | undefined;
 };
 
+function autoScroll(
+  container: HTMLDivElement | null,
+  speed: number,
+  initialAmount: number = 0
+) {
+  if (!container) return;
+
+  let amount = initialAmount + speed;
+
+  // 現在のスクロール位置を更新
+  container.style.transform = `translateX(${amount}px)`;
+
+  // スクロール位置が要素の幅を超えたら0に戻す
+
+  if (speed > 0 && amount >= 0) {
+    amount = -container.getBoundingClientRect().width / 2;
+  } else if (
+    speed < 0 &&
+    amount <= -(container.getBoundingClientRect().width / 2)
+  ) {
+    amount = 0;
+  }
+
+  // スクロールの開始
+  requestAnimationFrame(() => autoScroll(container, speed, amount));
+}
+
 export const MemberList = ({ members }: MemberListProps) => {
   const [displayMembers, setDisplayMembers] = useState<MembersType[]>(
     members?.contents || []
   );
 
+  const listRef1 = useRef<HTMLDivElement>(null);
+  const listRef2 = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
-    const handleScroll = (e: any) => {
-      const scrollWidth = e.target.scrollWidth;
-      const scrollLeft = e.target.scrollLeft;
-      const clientWidth = e.target.clientWidth;
+    autoScroll(listRef2.current, -1);
 
-      // スクロール位置が右端に達した場合
-      if (scrollLeft + clientWidth >= scrollWidth - 100) {
-        // 既存のデータを再度追加
-        setDisplayMembers([...displayMembers, ...(members?.contents || [])]);
-      }
-    };
+    const list1 = listRef1.current;
 
-    document
-      .querySelector(".overflow-x-auto")
-      ?.addEventListener("scroll", handleScroll);
+    if (!list1) return;
 
-    return () =>
-      document
-        .querySelector(".overflow-x-auto")
-        ?.removeEventListener("scroll", handleScroll);
-  }, [displayMembers]);
+    autoScroll(
+      listRef1.current,
+      1,
+      listRef1.current.getBoundingClientRect().width / 2
+    );
+  }, []);
 
   // 同様に2分割するロジックはそのまま
   const halfLength = Math.ceil(displayMembers.length / 2);
@@ -41,10 +62,10 @@ export const MemberList = ({ members }: MemberListProps) => {
   const secondHalf = displayMembers.slice(halfLength);
 
   return (
-    <div className="overflow-x-auto p-6">
-      <div className="flex space-x-8">
-        {firstHalf.map((member, index) => (
-          <div key={index} className="min-w-max">
+    <div className="overflow-hidden w-full space-y-6 py-2">
+      <div className="flex w-max" ref={listRef1}>
+        {[...firstHalf, ...firstHalf].map((member, index) => (
+          <div key={index} className="min-w-max mr-6">
             <Member
               imageUrl={member.imageUrl ? member.imageUrl.url : ""}
               name={member.name}
@@ -57,20 +78,22 @@ export const MemberList = ({ members }: MemberListProps) => {
           </div>
         ))}
       </div>
-      <div className="flex space-x-8 ml-40 mt-4">
-        {secondHalf.map((member, index) => (
-          <div key={Number(index) + halfLength} className="min-w-max">
-            <Member
-              imageUrl={member.imageUrl ? member.imageUrl.url : ""}
-              name={member.name}
-              grade={member.age.join(", ")}
-              role={member.role.join(", ")}
-              github={member.github}
-              twitter={member.twitter}
-              instagram={member.instagram}
-            />
-          </div>
-        ))}
+      <div className="flex w-max" ref={listRef2}>
+        {[...secondHalf, ...secondHalf, ...secondHalf, ...secondHalf].map(
+          (member, index) => (
+            <div key={Number(index) + halfLength} className="min-w-max mr-6">
+              <Member
+                imageUrl={member.imageUrl ? member.imageUrl.url : ""}
+                name={member.name}
+                grade={member.age.join(", ")}
+                role={member.role.join(", ")}
+                github={member.github}
+                twitter={member.twitter}
+                instagram={member.instagram}
+              />
+            </div>
+          )
+        )}
       </div>
     </div>
   );
