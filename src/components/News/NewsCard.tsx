b55174aec544fa66876417b550b3936af0a28500
Kazuma Ito <zarigani2x@gmail.com>
2023-11-06 06:17:10 +0900

    style: Newsのスタイルを更新

--- src/components/News/NewsCard.tsx
+++ src/components/News/NewsCard.tsx
@@ -12,12 +12,12 @@ export const NewsCard = (props: Props) => {
   const { title, place, imageURL, isRight } = props;
 
   return (
-    <div className="z-10 flex w-full flex-col overflow-hidden bg-white shadow md:flex-row-reverse md:justify-between md:rounded-3xl">
-      <div className="relative h-40 min-h-full w-full overflow-hidden bg-neutral-50 md:w-1/3">
+    <div className="z-10 flex w-full flex-col overflow-hidden bg-white shadow sm:justify-between sm:rounded-3xl  sm:group-odd/news-item:flex-row-reverse sm:group-even/news-item:flex-row">
+      <div className="relative h-40 min-h-full w-full overflow-hidden bg-neutral-50 sm:w-1/3">
         <Image src={imageURL} width={800} height={800} alt="" />
       </div>
-      <div className="flex flex-col justify-between gap-3 p-6 md:w-2/3 md:p-10">
-        <div className="text-lg font-semibold">{title}</div>
+      <div className="flex flex-col justify-between gap-3 p-6 sm:w-2/3 sm:p-8">
+        <div className="text-lg font-semibold sm:text-xl">{title}</div>
         <div className="-ml-1 flex items-center gap-1 text-sm text-neutral-400">
           <Image
             src={"./images/mapping.svg"}
--- src/components/News/NewsList.tsx
+++ src/components/News/NewsList.tsx
@@ -10,20 +10,26 @@ type NewsCardListProps = {
 export const NewsList = ({ newsCards }: NewsCardListProps) => {
   const contents = newsCards?.contents ?? [];
   return (
-    <div className="relative w-full">
+    <div className="relative w-full sm:px-4">
       {contents.map((newsCard: NewsType, index) => (
         <div
           key={index}
-          className="relative mb-14 flex flex-col items-center first:pt-10"
+          className="group/news-item relative mb-10 flex flex-col first:pt-10"
         >
-          <div className="relative mb-6 grid w-full grid-cols-[auto,24px,auto] items-center text-center">
+          <div className="relative mb-4 grid w-full grid-cols-[1fr,24px,1fr] items-center text-center">
             {/* dot */}
             <div className="col-span-full row-start-1 flex justify-center">
               <div className="z-40 h-6 w-6 rounded-full border-[6px] border-background-object bg-white"></div>
             </div>
+
             {/* date */}
-            <div className="col-start-1 row-start-1 text-2xl font-bold">
-              {newsCard.eventDate.split("T")[0].split("-").slice(1).join("/")}
+            <div className="col-start-1 row-start-1 ml-auto flex items-center gap-3 pr-6 font-title group-even/news-item:col-start-3 group-even/news-item:ml-6 group-even/news-item:mr-auto">
+              <div className="hidden font-semibold text-red-500 group-first/news-item:block">
+                NEW!
+              </div>
+              <div className="text-2xl font-bold text-title">
+                {newsCard.eventDate.split("T")[0].split("-").slice(1).join("/")}
+              </div>
             </div>
           </div>
 
