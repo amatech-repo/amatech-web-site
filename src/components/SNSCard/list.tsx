62399016cba4f4ddd7fd84d16f67c46504cef583
Kazuma Ito <zarigani2x@gmail.com>
2023-11-14 23:39:31 +0900

    style: SNS, Sponsorのカードスタイルを更新

--- src/components/SNSCard/list.tsx
+++ src/components/SNSCard/list.tsx
@@ -3,9 +3,9 @@ import { SNSCard } from ".";
 
 export const SNSCardList = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-5 sm:flex-row">
+    <div className="flex flex-wrap items-center justify-center gap-5 px-4">
       <SNSCard snsName={"github"} id={"amatech-repo"} name={"GitHub"} />
-      <SNSCard snsName={"twitter"} id={"amatech1006"} name={"Twitter"} />
+      <SNSCard snsName={"x"} id={"amatech1006"} name={"X"} />
       <SNSCard snsName={"qiita"} id={"amatech1006"} name={"Qiita"} />
     </div>
   );
--- src/components/Sponsors/SponsorLogo.tsx
+++ src/components/Sponsors/SponsorLogo.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export const SponsorLogo = ({ ImagePath }: Props) => {
   return (
-    <div className="grid h-40 w-40 place-content-center rounded-[32px] bg-white p-6 shadow-xl sm:h-52 sm:w-52 sm:p-8">
+    <div className="grid h-36 w-36 place-content-center rounded-3xl bg-white p-6 shadow-md md:h-52 md:w-52 md:rounded-32 md:p-8">
       {ImagePath && <Image src={ImagePath} alt="" width={160} height={160} />}
     </div>
   );
